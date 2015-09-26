65b44b2eab5b961aacc0ce614e283b6b8ff30748
Raj <rajmuhar12@gmail.com>
2015-09-26 14:42:10 -0700

    Tipster version 1 from MobileBridge

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -6,14 +6,28 @@
 //  Copyright © 2015 Rajpreet Muhar. All rights reserved.
 //
 
+//hi, theses are comments, put anywhere
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    //blue laser, click and control
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billAmountField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tipAmountLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +35,30 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onEditingChange(sender: AnyObject) {
+        let tipPercentages = [0.18, 0.20, 0.23]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        
+        //let tipPercentage = tipPercentages[0]
+        //var billAmount = billAmountField.text
+        //var tipAmount = NSString(string: billAmount!).doubleValue * 0.2
+        //print(tipAmount)
+        
+        let billAmount = NSString(string: billAmountField.text!).doubleValue
+        let tipAmount = billAmount * tipPercentage
+        let total = billAmount + tipAmount
+        
+        //print(tipAmount)
+        //print(total)
+        //print("")
+        
+        tipAmountLabel.text = "$\(tipAmount)"
+        totalLabel.text = "$\(total)"
+        
+        
+        //text field only text, if need numbers, use escape,\, or double value
+        
+        print(tipControl.selectedSegmentIndex)
+    }
 }
 
